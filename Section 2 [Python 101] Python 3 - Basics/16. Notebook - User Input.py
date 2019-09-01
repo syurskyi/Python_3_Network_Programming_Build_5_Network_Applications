3ce33a1b39d2a62dcf537ef49a3c9f82e0430353
Grey <sergejyurskyj@yahoo.com>
2019-09-01 13:40:31 -0700

    Commit Section 2 [Python 101] Python 3 - Basics

---
+++ Section 2 [Python 101] Python 3 - Basics/16. Notebook - User Input.py
@@ -0,0 +1,6 @@
+# User input
+input("Please enter the string you want to be printed out: ")
+
+# Saving the input to a variable
+user_says = input("Please enter the string you want to be printed out: ")
+
